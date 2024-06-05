86957366a67743560712baf60243550a970b2e1c
Jonathan Spurling <jon@jonspurling.ca>
2024-06-04 23:54:52 -0500

    Fixed CTA in Hero to go to Projects

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import MyPortrait from '../assets/my-portrait.png';
+import { HashLink as Link } from 'react-router-hash-link';
 
 const Hero = () => {
   return (
     <section className='hero'>
-      <div className="hero-content" data-aos="zoom-in"> {/* Wrapper for content */}
-        <div className="firstPanel">  {/* Wrapper for text */}
+      <div className="hero-content" data-aos="zoom-in"> 
+        <div className="firstPanel"> 
           <h1>
             Hi!<br className='desktop'/> I'm<span className='name'>&nbsp; Jon</span>
           </h1>
-          <button className="hero-cta-button desktop">See My Work</button>
+          <Link smooth to="#projects" className="hero-cta-button desktop">See My Work</Link> 
         </div>
-        <div className="hero-image-container">  {/* Separate container for image */}
-          <img src={MyPortrait} alt="Jon Spurling aka Happy Coder"  className="hero-image" />
+
+        <div className="hero-image-container"> 
+          <img src={MyPortrait} alt="Jon Spurling aka Happy Coder" className="hero-image" />
         </div>
+
         <p className="hero_description">
           An ever evolving full-stack developer.
         </p>
-        <button className="hero-cta-button mobile">See My Work</button>
+
+        <Link smooth to="#projects" className="hero-cta-button mobile">See My Work</Link>
       </div>
     </section>
   );
